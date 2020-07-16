036484a33f6126c4d4e503f0cd43e6b3b910fbb4
David Temple <dstemple7@gmail.com>
2020-07-16 15:05:31 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+import ProductContext from './contexts/ProductContext'
+import CartContext from './contexts/CartContext'
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import Product from './components/Product';
 
 function App() {
 	const [products] = useState(data);
@@ -13,20 +17,28 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart({
+			...cart,
+			item
+		})
 	};
 
 	return (
 		<div className="App">
+			<ProductContext.Provider value={{products, addItem}}>
+			<CartContext.Provider value={{cart}}>
 			<Navigation cart={cart} />
 
 			{/* Routes */}
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				<Products />
 			</Route>
 
 			<Route path="/cart">
 				<ShoppingCart cart={cart} />
 			</Route>
+			</CartContext.Provider>
+			</ProductContext.Provider>
 		</div>
 	);
 }
